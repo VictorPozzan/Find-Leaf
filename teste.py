8ffd955edd4d44156a606e38bdca2c52c1fbc69c
Victor Augusto Pozzan <Crazy-Jaguar@localhost.localdomain>
2020-12-14 16:12:02 -0300

    funcao nao otimizada de salvar imagem colorida

--- teste.py
+++ teste.py
@@ -133,9 +133,7 @@ def grayscale(img):
 
     return img
 
-
-def init():
-    imagem = cv2.imread('Teste02.png')
+def remove_ruidos(imagem):
     img = imagem.astype('float')
     img = img[:,:,0] # convert to 2D array
  
@@ -146,9 +144,12 @@ def init():
     img = cv2.medianBlur(img, 5)
     
     cv2.imwrite("saida1.png", gray_img)   
-    cv2.imwrite("saida2.png", img)   
+    cv2.imwrite("saida2.png", img)
 
-    avg = np.mean(imagem,axis=-1)
+    return img   
+
+def init(img):
+    #avg = np.mean(imagem,axis=-1)
     
     listaDeFronteiras=[]
 
@@ -179,17 +180,17 @@ def init():
     print("NUMERO DE FOLHAS ENCONTRADAS:")
     print(len(listaDeFronteiras))
 
-    contour = np.zeros((imagem.shape)) #cria uma matriz do tamanho da imagem preenchida com zero
-    for i in listaDeFronteiras:
-        for pixel in range(len(i)):
-            contour[i[pixel][0]][i[pixel][1]] = [255,255,255]
+    #contour = np.zeros((imagem.shape)) #cria uma matriz do tamanho da imagem preenchida com zero
+    #for i in listaDeFronteiras:
+    #    for pixel in range(len(i)):
+    #        contour[i[pixel][0]][i[pixel][1]] = [255,255,255]
     
 
     #cv2.imshow('Image', imagem)
     #cv2.imshow('Image_contour', contour)
-    cv2.imwrite("saida.png", contour)   
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.imwrite("saida.png", contour)   
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
     return listaDeFronteiras
 
@@ -205,7 +206,7 @@ def encontra_dimensoes(fronteira):
     y_menor = min(list_y) 
     y_maior = max(list_y)
 
-    return (x_maior-x_menor)+3 , (y_maior-y_menor)+3, x_menor, y_menor
+    return (x_maior-x_menor)+3 , (y_maior-y_menor)+3, x_menor, y_menor, x_maior, y_maior
 
 def criar_imagem_borda(img_borda, fronteira, menor_x, menor_y, index):
 
@@ -221,27 +222,95 @@ def criar_imagem_borda(img_borda, fronteira, menor_x, menor_y, index):
     cv2.imwrite(nome_imagem, img_borda)   
 
 
-def recorta_imagem(lista_fronteiras):
+def getColor(pixel, imagem_original, pos_x, pos_y):
+    RGB = []
+    #pegar a posição do pixel na imagem
+    #posicao_do_pixel = posicao_pixel(imagem_original, pixel)
+    pos_x = pixel[0] + 1
+    pos_y = pixel[1] + 1 
 
+    return imagem_original[pos_x,pos_y], pos_x, pos_y
+
+def encontra_pixel(fronteira, encontrou_borda, img, pixel, col):
+    x = pixel[0]
+    y = pixel[1]
+
+    #se encontrar um ponto na fronteira pegar a 
+    #cor indicar que entro na imagem encontrou_borda = true            
+    if boolPontoNaBorda((x,y), fronteira):
+        encontrou_borda = True
+        #caso ele seja a borda de saída
+        if img[x, y+1] >= BRANCO:
+            encontrou_borda = False
+            x += 1
+            y = 0
+            while(boolPontoNaBorda((x,y), fronteira) == False):
+                y += 1
+                if(y>col):
+                    return (0,0), False, encontrou_borda         
+        if encontrou_borda:
+            y += 1
+
+        return (x,y), True, encontrou_borda
+    
+    elif encontrou_borda and boolPontoNaBorda((x,y), fronteira) == False:
+        y += 1
+        return (x,y), True, encontrou_borda
+    
+        
+    return (0,0), False, encontrou_borda
+
+def criar_imagem_unica_folha_colorida(img, imagem_original, imagem_branca, fronteira, menor_x, menor_y, maior_x, maior_y, index):
+    #criar uma lista de pontos das coordenadas das cores
+    print("função de criar imagem única colorida")
+    encontrou_borda = False
+    row, col = img.shape        
+    pos_x = 0
+    pos_y = 0
+    esta_pintando = True
+    pixel = fronteira[0]
+    while(esta_pintando):
+        cor_RGB, pos_x, pos_y = getColor(pixel, imagem_original, pos_x, pos_y)
+        nova_coordenada_x = (int(pixel[0])-menor_x)+1
+        nova_coordenada_y = (int(pixel[1])-menor_y)+1
+        imagem_branca[nova_coordenada_x][nova_coordenada_y][0] = cor_RGB[0]
+        imagem_branca[nova_coordenada_x][nova_coordenada_y][1] = cor_RGB[1]
+        imagem_branca[nova_coordenada_x][nova_coordenada_y][2] = cor_RGB[2]
+        pixel, esta_pintando, encontrou_borda = encontra_pixel(fronteira, encontrou_borda, img, pixel, col)
+
+    nome_imagem = 'ImagemColorida' + str(index) + ".png"
+    print("Salvou a imagem: ", index)
+    cv2.imwrite(nome_imagem, imagem_branca)   
+            
+
+
+def recorta_imagem(lista_fronteiras, imagem_sem_ruido, imagem_original):
+    
     print("FUNÇÃO DE RECORTAR IMAGEM")
     index = 0
     for fronteira in lista_fronteiras:
-        row, col, menor_x, menor_y = encontra_dimensoes(fronteira)#encontar a largura e a altura 
+        row, col, menor_x, menor_y, maior_x, maior_y = encontra_dimensoes(fronteira)#encontar a largura e a altura 
         
         #salavando a borda
         imagem_branca = np.ones((row, col, 3)) * 255
         cv2.imwrite("Branco.png", imagem_branca)   
-
-        criar_imagem_borda(imagem_branca, fronteira, menor_x, menor_y, index)
+        
+        #criar_imagem_borda(imagem_branca, fronteira, menor_x, menor_y, index)
+        
+        #salvando a folha
+        criar_imagem_unica_folha_colorida(imagem_sem_ruido, imagem_original, imagem_branca, fronteira, menor_x, menor_y, maior_x, maior_y, index)
         index += 1 
 
 
 def main():
+    imagem = cv2.imread('Folhas/Teste10.png')
+    #imagem = cv2.imread('teste-separar.png')
+    img_sem_ruido = remove_ruidos(imagem)
     print("Bem Vindo ao Trabalho de Processamento de Imagens")
     lista_fronteiras = []
     #while(processar todas as imagens da pasta Folhas)
-    lista_fronteiras = init() #encontrar as bordas
-    recorta_imagem(lista_fronteiras) # recortar as folhas e salvar em disco (salvar a borda e a folha original)
+    lista_fronteiras = init(img_sem_ruido) #encontrar as bordas
+    recorta_imagem(lista_fronteiras, img_sem_ruido, imagem) # recortar as folhas e salvar em disco (salvar a borda e a folha original)
     #analise_textura() #
 
     #criar_planilha() # salvar os dados 
